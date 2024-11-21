9cdfda1f72a9db3d0a8a0642da9912a53ac40dec
djeymiquel-dev <djey.miquel.developer@gmail.com>
2024-11-21 02:20:32 +0100

    exercise onChange

--- src/components/DrinkSearch.jsx
+++ src/components/DrinkSearch.jsx
@@ -6,13 +6,21 @@ import { DrinkList } from "./DrinkList";
 import { availableDrinks } from "../../utils/data";
 
 export const DrinkSearch = ({ clickFn }) => {
-  const [searchField, useSearchField] = useState("test-drink");
+  const [searchField, setSearchField] = useState("");
+
+  const matchedDrinks = availableDrinks.filter((drink) => {
+    return drink.name.toLowerCase().includes(searchField.toLowerCase());
+  });
+
+  const handleChange = (event) => {
+    setSearchField(event.target.value);
+  };
 
   return (
     <>
       <label>Search for Drinks: </label>
-      <TextInput />
-      <DrinkList clickFn={clickFn} drinks={availableDrinks} />
+      <TextInput changeFn={handleChange} />
+      <DrinkList clickFn={clickFn} drinks={matchedDrinks} />
     </>
   );
 };
--- src/components/UI/TextInput.jsx
+++ src/components/UI/TextInput.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/react-in-jsx-scope */
 import "./TextInput.css";
-export const TextInput = () => {
+export const TextInput = ({ changeFn }) => {
   return (
     <>
-      <input className="text-input"></input>
+      <input className="text-input" onChange={changeFn}></input>
     </>
   );
 };
